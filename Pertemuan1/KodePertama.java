906d5e204aafb0d86d7965aa6b198aa1de99b980
NaufrisyaRizqySyabilla <d1041241081@student.untan.ac.id>
2026-02-05 15:36:41 +0700

    Pertemuan1

---
+++ Pertemuan1/KodePertama.java
@@ -0,0 +1,11 @@
+public class KodePertama {
+public static void main(String[] args){
+        System.out.println("Halo, Pemrograman Berorientasi Objek!");
+        System.out.println("Ini adalah program Java pertama saya.");
+    }
+}
+/*
+Naufrisya Rizqy Syabilla
+D1041241081
+Kamis, 5 Februari 2025
+ */
